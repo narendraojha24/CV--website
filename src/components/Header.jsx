b8d04ae06e4773177d1ea2e516be4db09d6818c5
narendraojha24 <151997177+narendraojha24@users.noreply.github.com>
2025-03-31 19:39:24 +0530

    commit

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,8 +2,11 @@ import React from 'react'
 
 export default function Header({darkMode, toggleDarkMode}) {
   return (
+    <>
     <div className='min-h-screen flex-1 relative p-10'>
-    Header content should be here
+    Header content should be herecdscs fgnrtddddddddddddddddddddddddddddddddddddddddddddddddddddddddddtr jr6  yjj56uuuuuuuuuuuuuuuuuuuuuuuuuuuuuuubu
     </div>
+    <div>rlfno4</div>
+    </>
   )
 }
