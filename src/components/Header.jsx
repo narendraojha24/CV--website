73c582b9e6c73887d0e4d9438ad74ebb735de1c7
narendraojha24 <151997177+narendraojha24@users.noreply.github.com>
2025-04-09 22:42:57 +0530

    added about section

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,9 +1,146 @@
 import React from 'react'
+import { BiMoon } from 'react-icons/bi'
+import { BiSun } from 'react-icons/bi'
+import image from '/public/image.jpg'
+import { BiCurrentLocation, BiEnvelope, BiPhone } from 'react-icons/bi'
+import { BiLogoGithub, BiLogoTwitter, BiLogoLinkedin } from 'react-icons/bi'
 
 export default function Header({darkMode, toggleDarkMode}) {
   return (
-    <div className='min-h-screen flex-1 relative p-10'>
-    Header
+    <div className="min-h-screen flex-1 relative p-10 transition-all duration-500 dark:bg-black dark:text-white sm:border-r dark:border-gray-700">
+
+    <div className="flex flex-col gap-5">
+
+      <div className="flex flex-col items-center justify-center gap-5">
+        <img src={image} alt="" className="w-32 rounded-full"/>
+
+        <div className="text-center space-y-1">
+          <h1 className="text-4xl font-light">
+            Chris <span className="font-semibold">Josh</span>
+          </h1>
+          <h3 className="text-xl font-light">Web Developer</h3>
+        </div>
+
+        <button className='absolute right-10 top-10' onClick={toggleDarkMode}>
+          {
+            darkMode ? (
+              <BiSun className='text-2xl'/>
+            ) : (
+              <BiMoon className='text-2xl' />
+            )
+          }
+        
+        </button>
+      </div>
+
+      <div className="flex flex-col items-start justify-start gap-5 border-b p-5 dark:border-gray-700">
+  <ul className="space-y-3">
+    <li className="flex items-center gap-2">
+      <BiCurrentLocation className="text-xl" />
+      <span>Williams St. 256 Los Angeles</span>
+    </li>
+    <li className="flex items-center gap-2">
+      <BiEnvelope className="text-xl" />
+      <span>user@gmail.com</span>
+    </li>
+    <li className="flex items-center gap-2">
+      <BiPhone className="text-xl" />
+      <span>123-456-789</span>
+    </li>
+  </ul>
+</div>
+
+<div className="flex flex-col items-start justify-start gap-5 border-b p-5 dark:border-gray-700">
+  <h1 className="text-base font-semibold md:text-2xl">SOCIAL</h1>
+
+  <ul className="space-y-3">
+    <li className="flex items-center gap-2">
+      <BiLogoGithub className="text-xl" />
+      <span>@githubuser</span>
+    </li>
+    <li className="flex items-center gap-2">
+      <BiLogoTwitter className="text-xl" />
+      <span>@twitteruser</span>
+    </li>
+    <li className="flex items-center gap-2">
+      <BiLogoLinkedin className="text-xl" />
+      <span>@linkedinuser</span>
+    </li>
+  </ul>
+</div>
+
+<div className="flex flex-col items-start justify-start gap-5 border-b p-5 dark:border-gray-700">
+  <h1 className="text-base font-semibold md:text-2xl">EDUCATION</h1>
+
+  <ul>
+    <li className="relative flex flex-col gap-1 border-l p-6 before:absolute before:left-[-6px] before:top-1/2 before:h-3 before:w-3 before:-translate-y-1/2 before:rounded-full before:bg-black dark:before:bg-white before:transform">
+      <span className="font-semibold md:text-lg">Computer Science</span>
+      <span className="font-light">University of X</span>
+      <span className="text-sm text-gray-400">2016–2020</span>
+    </li>
+
+    <li className="relative flex flex-col gap-1 border-l p-6 before:absolute before:left-[-6px] before:top-1/2 before:h-3 before:w-3 before:-translate-y-1/2 before:rounded-full before:bg-black dark:before:bg-white before:transform">
+      <span className="font-semibold md:text-lg">Computer Science</span>
+      <span className="font-light">University of X</span>
+      <span className="text-sm text-gray-400">2016–2020</span>
+    </li>
+
+  </ul>
+</div>
+
+<div className="flex flex-col items-start justify-start gap-5 border-b p-5 dark:border-gray-700">
+  <h1 className="text-base font-semibold md:text-2xl">SKILLS</h1>
+
+  <div className="flex flex-wrap gap-3">
+    <div className="flex flex-wrap gap-3">
+      <span className="rounded-xl bg-black text-white p-3 text-center dark:bg-white dark:text-black">HTML</span>
+      <span className="rounded-xl bg-black text-white p-3 text-center dark:bg-white dark:text-black">CSS</span>
+      <span className="rounded-xl bg-black text-white p-3 text-center dark:bg-white dark:text-black">JavaScript</span>
+      <span className="rounded-xl bg-black text-white p-3 text-center dark:bg-white dark:text-black">React</span>
     </div>
+  </div>
+  <div className="flex flex-wrap gap-3">
+    <div className="flex flex-wrap gap-3">
+      <span className="rounded-xl bg-black text-white p-3 text-center dark:bg-white dark:text-black">HTML</span>
+      <span className="rounded-xl bg-black text-white p-3 text-center dark:bg-white dark:text-black">CSS</span>
+      <span className="rounded-xl bg-black text-white p-3 text-center dark:bg-white dark:text-black">JavaScript</span>
+      <span className="rounded-xl bg-black text-white p-3 text-center dark:bg-white dark:text-black">React</span>
+    </div>
+  </div>
+</div>
+
+<div className="flex flex-col items-start justify-start gap-5 border-b p-5 dark:border-gray-700">
+  <h1 className="text-base font-semibold md:text-2xl">HOBBIES</h1>
+
+  <div className="flex flex-wrap gap-16 p-3">
+    <ul className="flex list-disc flex-col gap-3">
+      <li>Skiing</li>
+      <li>Reading Books</li>
+      <li>Workout</li>
+    </ul>
+
+    <ul className="flex list-disc flex-col gap-3">
+      <li>Boxing</li>
+      <li>Hiking</li>
+      <li>Gaming</li>
+    </ul>
+  </div>
+</div>
+
+<div className="flex flex-col items-start justify-start gap-5 border-b p-5 dark:border-gray-700">
+  <h1 className="text-base font-semibold md:text-2xl">LANGUAGES</h1>
+
+  <ul className="flex list-disc flex-wrap gap-12 md:text-lg">
+    <li>English</li>
+    <li>German</li>
+    <li>Spanish</li>
+  </ul>
+</div>
+
+
+
+    </div>
+  </div>
   )
 }
+
--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -2,10 +2,29 @@ import React from 'react'
 
 export default function Home() {
   return (
-    <div className='min-h-screen flex-[3]'>
-        homeb hxued hixce jhxste cyygd33h gc
-        <h2>ndkcjoej</h2>
-        <h1>knef3</h1>
+    <div className="min-h-screen flex-[3] transition-all duration-500 dark:bg-black dark:text-white">
+  <main className="flex max-w-[1000px] flex-col gap-10 p-5 md:mx-10 md:py-14">
+    <div className="space-y-5">
+      <h1 className="text-2xl font-bold md:text-4xl">About</h1>
+      <p className="text-sm text-gray-600 dark:text-gray-400 md:text-base">
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Perferendis
+        labore cum cupiditate debitis, dignissimos aliquam quisquam repudiandae
+        vel ratione omnis ex odit aperiam explicabo vero repellat magnam saepe
+        recusandae at consequatur nemo ipsam rem quidem. Reprehenderit,
+        voluptatem excepturi, quam enim pariatur neque, debitis nam voluptate
+        aliquid sit quia error repellendus doloremque molestiae! Officiis facere
+        nostrum eaque aperiam architecto vel at non possimus expedita explicabo?
+        Necessitatibus, quasi cumque dignissimos ex quae iusto ex rem enim
+        blanditiis, dicta soluta nemo dolorem repellendus similique repudiandae
+        at corporis eius repellat natus nisi? Ullam velit repellendus obcaecati
+        dignissimos eligendi porro minus quo expedita officiis, dolor
+        consectetur!
+      </p>
     </div>
+
+    
+  </main>
+</div>
+
   )
 }
