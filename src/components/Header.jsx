e04bf9ef40635f25146978e7b4f757f0e0befe94
narendraojha24 <151997177+narendraojha24@users.noreply.github.com>
2025-05-10 21:05:02 +0530

    changes

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -76,7 +76,7 @@ export default function Header({darkMode, toggleDarkMode}) {
   return (
     <div className="min-h-screen flex-1 relative p-10 transition-all duration-500 dark:bg-black dark:text-white sm:border-r dark:border-gray-700" id='home'>
 <div className="fixed top-6 left-4 z-50 sm:hidden">
-  <div className="relative w-14 h-14 rounded-full bg-black text-white dark:bg-white dark:text-black shadow-lg shadow-gray-900/50 flex items-center justify-center text-4xl font-bold">N.</div>
+  <div className="relative w-14 h-14 rounded-full bg-opacity-40 dark:bg-opacity-40 transparent bg-white text-black dark:bg-black dark:text-white flex items-center justify-center text-4xl font-bold">N.</div>
 </div>
 
 {/*
@@ -125,7 +125,7 @@ export default function Header({darkMode, toggleDarkMode}) {
     */}
 
 <div
-  className="fixed bottom-8 left-4 right-4 bg-black dark:bg-white rounded-3xl shadow-lg shadow-gray-900/50 flex justify-around items-center py-3 z-50 sm:hidden"
+  className="fixed bottom-8 left-4 right-4 bg-black bg-opacity-90 dark:bg-opacity-80 dark:bg-white rounded-3xl shadow-lg shadow-gray-900/50 flex justify-around items-center py-3 z-50 sm:hidden"
 >
   {icons.map((item, index) => (
     <a
